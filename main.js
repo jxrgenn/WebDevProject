5a4aae1316901ca521a70dff19708e5ea78adf21
Jurgen Halili <117214911+jxrgenn@users.noreply.github.com>
2023-06-08 05:18:05 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// JavaScript
-document.getElementById('loginBtn').addEventListener('click', function() {
-    window.open('Login.html');
-});
-
-document.getElementById('registerBtn').addEventListener('click', function() {
-    window.open('Register.html');
-});
