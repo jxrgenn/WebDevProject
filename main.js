e152ccac98526bb82a1b56ee3472ba937935b4c7
Endi <99030501+5H1M4@users.noreply.github.com>
2023-06-08 22:35:54 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// JavaScript
+document.getElementById('loginBtn').addEventListener('click', function() {
+    window.open('Login.html');
+});
+
+document.getElementById('registerBtn').addEventListener('click', function() {
+    window.open('Register.html');
+});
